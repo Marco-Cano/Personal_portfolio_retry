8806ab250385a5eef55d3f3de89a869ee8f98529
Marco Cano <marco62412a@gmail.com>
2021-06-18 20:06:15 -0500

    before nick's bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+<<<<<<< HEAD
 # Create your models here.
+=======
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=None)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
+>>>>>>> 1bf9651 (trying to display favicon)
